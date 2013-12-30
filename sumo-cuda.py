304cd281d690f445f50c2845a17fa06779682710
Chris Blatchley <chris@chrisblatchley.com>
2013-12-30 12:05:18 -0500

    spaces default

--- sumo-cuda.py
+++ sumo-cuda.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
 help_string = """SUMO-CUDA
-	USAGE: python sumo-cuda.py [options] network.netccfg
+    USAGE: python sumo-cuda.py [options] network.netccfg
 
 Authors: Thaddeus Bond, Chris Blatchley
 """
 
 def main():
-	print help_string
+    print help_string
 
 
 if __name__ == '__main__':
-	main()
+    main()
